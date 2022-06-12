c829f33bfc01b7fec3a04a052bca6a11038e4067
RoozbehZhuleh <zhuleh.developer@gmail.com>
2022-06-13 00:09:44 +0430

    major update
    added mobile responsive

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-mobile = document.querySelector("#mobile");
-process = document.querySelector("#our-process");
-customize = document.querySelector("#customize");
-webDev = document.querySelector("#web-development");
+let width = screen.width;
+const mobile = document.querySelector("#mobile");
+const process = document.querySelector("#our-process");
+const customize = document.querySelector("#customize");
+const webDev = document.querySelector("#web-development");
 
 const mobileScrollFunc = function () {
     let y = window.scrollY;
-    if (y >= 400) {
+    if (y >= 400 && width > 1000) {
+
         mobile.classList.add("animate__fadeInRight");
         mobile.classList.remove("animate__fadeOutRight");
         process.classList.add("animate__fadeInLeft");
@@ -14,7 +16,7 @@ const mobileScrollFunc = function () {
         customize.classList.remove("animate__fadeOutLeft");
          webDev.classList.add("animate__fadeInLeft");
         webDev.classList.remove("animate__fadeOutLeft");
-    } else if (y <= 100) {
+    } else if (y <= 100 && width > 1000) {
         mobile.classList.add("animate__fadeOutRight");
         mobile.classList.remove("animate__fadeInRight");
         process.classList.add("animate__fadeOutLeft");
@@ -24,21 +26,23 @@ const mobileScrollFunc = function () {
         webDev.classList.add("animate__fadeOutLeft");
         webDev.classList.remove("animate__fadeInLeft");
     }
-    else if (y >= 1000) {
-       
-    }
-    else if (y >= 950) {
+
+    else if (width < 900) {
+        mobile.classList.remove("animate__animated");
+        customize.classList.remove("animate__animated");
+        process.classList.remove("animate__animated");
+        webDev.classList.remove("animate__animated");
     }
 }
 window.addEventListener("scroll", mobileScrollFunc);
 
 
 
+// burger menu
+const burgerIcon = document.querySelector(".burger__icon");
+const burgerMenu = document.querySelector(".burger__menu");
 
-// const processScrollFunc = function () {
-//     let y = window.scrollY;
-// };
-// window.addEventListener("scroll", processScrollFunc);
-
-
-
+burgerIcon.addEventListener("click", () => {
+    burgerIcon.classList.toggle("rotate");
+    burgerMenu.classList.toggle("display--block")
+})
